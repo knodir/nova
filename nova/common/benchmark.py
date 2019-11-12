e1d13fbee271a9a1460dd4449acd2cf8f40e10b8
Marlon Ou <omzmarlon@gmail.com>
2019-11-11 20:52:14 -0800

    typo

--- nova/common/benchmark.py
+++ nova/common/benchmark.py
@@ -11,7 +11,7 @@ class NovaBenchmarker(object):
             f.close()
         with open(file_name, 'a+') as f:
             for benchmark in self.pending_benchmarks:
-                f.write("%s,%s,%s, %s\n" % benchmark)
+                f.write("%s,%s,%s,%s\n" % benchmark)
 
     def add_benchmark(self, req_id, vm_name, key, ignore):
         if not ignore:
